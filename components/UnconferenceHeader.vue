6cdd5587b5f2ff1fc238336702750742025a7705
Alain Hélaïli <helaili@github.com>
2025-10-21 10:10:15 +0200

    Refactor UnconferenceHeader component layout for improved structure and readability

--- components/UnconferenceHeader.vue
+++ components/UnconferenceHeader.vue
@@ -11,32 +11,37 @@
       <v-col cols="12" md="8">
         <h1 :class="$vuetify.display.smAndDown ? 'text-h3' : 'text-h2'" class="font-weight-bold mb-4 mb-md-6">{{ title }}</h1>
         
-        <v-card class="mx-auto pa-4 pa-md-6 mb-6 mb-md-8" max-width="800" elevation="3">
-          <div :class="$vuetify.display.smAndDown ? 'text-h6' : 'text-h5'" class="mb-4">
-            Join us for great discussions and knowledge sharing!
-          </div>
-          
-          <v-list>
-            <v-list-item prepend-icon="mdi-calendar">
-              <v-list-item-title :class="$vuetify.display.smAndDown ? 'text-body-1' : 'text-h6'">{{ date }}</v-list-item-title>
-            </v-list-item>
+        <v-card class="mx-auto" max-width="800" elevation="3">
+          <v-card-text class="pa-4 pa-md-6">
+            <div :class="$vuetify.display.smAndDown ? 'text-h6' : 'text-h5'" class="mb-4">
+              Join us for great discussions and knowledge sharing!
+            </div>
             
-            <v-list-item prepend-icon="mdi-map-marker">
-              <v-list-item-title :class="$vuetify.display.smAndDown ? 'text-body-1' : 'text-h6'">{{ location }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
+            <v-list>
+              <v-list-item prepend-icon="mdi-calendar">
+                <v-list-item-title :class="$vuetify.display.smAndDown ? 'text-body-1' : 'text-h6'">{{ date }}</v-list-item-title>
+              </v-list-item>
+              
+              <v-list-item prepend-icon="mdi-map-marker">
+                <v-list-item-title :class="$vuetify.display.smAndDown ? 'text-body-1' : 'text-h6'">{{ location }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-card-text>
+          
+          <v-card-actions class="px-4 px-md-6 pb-4 pb-md-6">
+            <v-btn
+              color="primary"
+              :size="$vuetify.display.smAndDown ? 'large' : 'x-large'"
+              to="/login"
+              :class="$vuetify.display.smAndDown ? 'text-h6 px-6' : 'text-h6 px-8'"
+              :block="$vuetify.display.smAndDown"
+              class="mx-auto"
+              min-width="250"
+            >
+              Join the Discussion
+            </v-btn>
+          </v-card-actions>
         </v-card>
-        
-        <v-btn
-          color="primary"
-          :size="$vuetify.display.smAndDown ? 'large' : 'x-large'"
-          to="/login"
-          :class="$vuetify.display.smAndDown ? 'text-h6 px-6' : 'text-h6 px-8'"
-          block
-          max-width="300"
-        >
-          Join the Discussion
-        </v-btn>
       </v-col>
     </v-row>
   </v-container>
